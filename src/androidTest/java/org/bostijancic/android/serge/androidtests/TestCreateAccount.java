ed5f6564f3c4df678c3fb92b2b1d2a0bc38a88a5
emil <emil.bostijancic@gmail.com>
2014-03-08 01:58:12 +0100

    lot of javadoc used to learn how to use the framework.

--- src/androidTest/java/org/bostijancic/android/serge/androidtests/TestCreateAccount.java
+++ src/androidTest/java/org/bostijancic/android/serge/androidtests/TestCreateAccount.java
@@ -2,26 +2,39 @@ package org.bostijancic.android.serge.androidtests;
 
 import android.test.ActivityInstrumentationTestCase2;
 
-import com.google.android.apps.common.testing.ui.espresso.Espresso;
-import com.google.android.apps.common.testing.ui.espresso.action.ViewActions;
-import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
-
 import org.bostijancic.android.jenkdroid.R;
 import org.bostijancic.android.serge.MainActivity;
 
+/* some static imports to get human readable espresso test method invocations. */
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+
 /**
- * Created by ebostijancic on 07.03.14.
+ * simple test to test the MainActivity.
+ *
+ * ebostijancic on 07.03.14.
  */
 public class TestCreateAccount extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    public TestCreateAccount(Class<MainActivity> activityClass) {
-        super(activityClass);
+    // this public no-arg constructor is necessary for calling espresso tests.
+    public TestCreateAccount() {
+        super(MainActivity.class);
     }
 
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        // it's important to call getActivity(); otherwise an exception is thrown.
+        getActivity();
+    }
+
+    /**
+     * simple test just to see if espresso is working correctly.
+     */
     public void testOpenAccountView() {
-       Espresso.onView(ViewMatchers.withId(R.id.serverName)).perform(ViewActions.typeText("emil"));
-
-
+        onView(withId(R.id.serverName)).perform(typeText("emil"));
     }
 
 }
